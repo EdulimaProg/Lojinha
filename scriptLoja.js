cbfc31f0365f460111bdcfcdaffae05939956df6
Eduardo Lima <edulimaprog@gmail.comEduardo>
2019-06-09 08:31:57 -0300

    meu deus que coisa chata essa api heroku

--- scriptLoja.js
+++ scriptLoja.js
@@ -1,2 +1,16 @@
-var dadosJson = '';
+function lerJS(){
+$(document).ready(function () {
+    $.ajax({
+        type:'GET',
+        url:'https://bannetloja.herokuapp.com/produtos',
+        data:data,
+        dataType:'json',
+        success: function (data) {
+            $.each(data,function (index,element) {
+                console.log(element[index].nome);
+            })
 
+        }
+    })
+})
+}
